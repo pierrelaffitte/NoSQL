e7233fd947200737aa3eb19f93bf36454c8db014
pierrelaffitte <pierre.laffitte.mata@gmail.com>
2018-02-10 16:07:41 +0100

    modification du code python pour exécuter les requêtes mongo avec pymongo la première requête passe en console

--- mes_Requetes.py
+++ mes_Requetes.py
@@ -5,26 +5,12 @@ Created on Thu Feb  8 11:54:25 2018
 
 @author: pierre
 """
-
+import pymongo
+import json
+import pprint
 cnx_string = "mongodb://localhost:27017"
 cnx = pymongo.MongoClient(cnx_string)
 db = cnx.datas
-db = pymongo.MongoClient()
-result = db.things.insert_many([{"x": 1, "tags": ["dog", "cat"]},
-                                 {"x": 2, "tags": ["cat"]},
-                                 {"x": 2, "tags": ["mouse", "cat", "dog"]},
-                                 {"x": 3, "tags": []}])
-result.inserted_ids
-
-
-pipeline = [
-   {"$unwind": "$tags"},
-  {"$group": {"_id": "$tags", "count": {"$sum": 1}}},
- ]
-import pprint
-pprint.pprint(list(db.things.aggregate(pipeline)))
-
-
 ## ----------------------------------------------------------------------------
 # Requête 1
 ma_requete =  [ { 
@@ -33,5 +19,6 @@ ma_requete =  [ {
         "count": { "$sum": 1 }
       }
   }]
-  
+print("Requête 1 ") 
 pprint.pprint(list(db.hospitals.aggregate(ma_requete)))
+print("\n\n")
